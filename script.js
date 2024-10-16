b78860aa5109d0b6ff6b6e090294fae0e38d0f1d
asadbekumarov <asadbekumarov922@gmail.com>
2024-10-17 01:02:29 +0500

    endd

---
+++ script.js
@@ -0,0 +1,135 @@
+// 1-----------------------------------------------------------------
+// let yosh = +prompt("yoshni kiriting");
+// if (yosh < 18) {
+//   alert(`Siz voyaga yetmagansiz`);
+// } else if (yosh >= 18 && yosh < 60) {
+//   alert(`siz kattasiz`);
+// } else if (yosh > 60) {
+//   alert(`Siz keksa fuqorasiz`);
+// } else {
+//   alert("notogri qiymat kiritildi");
+// }
+// 2-----------------------------------------------------------------
+// let son = +prompt("1 dan 4 gacha son kiriting");
+// switch (son) {
+//   case 1:
+//     alert(`nonushta`);
+//     break;
+//   case 2:
+//     alert(`tushlik`);
+//     break;
+//   case 3:
+//     alert(`kechki ovqat`);
+//     break;
+//   case 4:
+//     alert(`gazak`);
+//     break;
+//   default:
+//     alert(`notogri qiymat kiritildi`);
+// }
+// 4-----------------------------------------------------------------
+// STRING METOD
+// let str = "Salom";
+// console.log(str.charAt(1));
+// ------------------------------------------------------------------
+// let str = "Salom";
+// console.log(str.length);
+// ------------------------------------------------------------------
+// let str = "salom";
+// console.log(str.toUpperCase());
+// ------------------------------------------------------------------
+// let str = "SALOM";
+// console.log(str.toLowerCase());
+// ------------------------------------------------------------------
+// let str = "Salom";
+// console.log(str.indexOf("l"));
+// ------------------------------------------------------------------
+// let str = "Salom";
+// console.log(str.includes("lo"));
+// ------------------------------------------------------------------
+// let str = "Salom";
+// console.log(str.slice(1, 4));
+// ------------------------------------------------------------------
+// let str = "Salom";
+// console.log(str.substring(1, 4));
+// ------------------------------------------------------------------
+// let str = "Salom, Dunyo";
+// let arr = str.split(", ");
+// console.log(arr);
+// ------------------------------------------------------------------
+// let str = "Salom Dunyo";
+// console.log(str.replace("Dunyo", "Olam"));
+// ------------------------------------------------------------------
+// let str = "   Salom   ";
+// console.log(str.trim());
+// ------------------------------------------------------------------
+// let str1 = "Salom";
+// let str2 = "Dunyo";
+// console.log(str1.concat(" ", str2));
+// ------------------------------------------------------------------
+// let str = "Salom";
+// console.log(str.startsWith("Sa"));
+// ------------------------------------------------------------------
+// let str = "Salom";
+// console.log(str.endsWith("m"));
+// ------------------------------------------------------------------
+// let str = "Salom";
+// console.log(str.repeat(3));
+// ------------------------------------------------------------------
+// let str = "5";
+// console.log(str.padStart(3, "0"));
+// ------------------------------------------------------------------
+// let str = "5";
+// console.log(str.padEnd(3, "0"));
+// ------------------------------------------------------------------
+// let str = "Asadbek";
+// console.log(str.charCodeAt(2));
+// ------------------------------------------------------------------
+// let str = "Salom Salom";
+// console.log(str.replaceAll("Salom", "aa"));
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// QO'SHIMCHA
+// const ball = +prompt("Ballingizni kiriting");
+// if (ball < 80) {
+//   console.log(`${ball} shu ball bilan o'qishga kira olamdingiz`);
+// } else if (ball > 80 && ball < 100) {
+//   confirm(
+//     `Siz ${ball} bilan super kantrakt asosida oqishga qabul qilindingz kontrakt miqdori yiliga 3000$ o'qishga rozimisz`
+//   );
+//   let pul = +prompt("pullingizni kirting");
+//   if (pul >= 3000) {
+//     confirm("Siz oqishga qabul qilindiniz");
+//   } else {
+//     confirm("pulimgiz yetmadi");
+//   }
+// } else if (ball >= 100 && ball < 150) {
+//   confirm(
+//     `Siz ${ball} bilan super kantrakt asosida oqishga qabul qilindingz kontrakt miqdori yiliga 2000$ o'qishga rozimisz`
+//   );
+//   let pul = +prompt("pullingizni kirting");
+//   if (pul >= 2000) {
+//     confirm("Siz oqishga qabul qilindiniz");
+//   } else {
+//     confirm("pulimgiz yetmadi");
+//   }
+// } else if (ball >= 150 && ball <= 180) {
+//   alert("siz grant bilan oqishga kirdingiz");
+// } else {
+//   alert("notog'ri qiymat kirtildi");
+// }
+// 2--------------------------------------------------------------------;
+// let yil = +prompt("yilni kiriting");
+// if (yil === 2024) {
+//   console.log("kabisa yili");
+// } else {
+//   console.log("kabisa yili emas");
+// }
